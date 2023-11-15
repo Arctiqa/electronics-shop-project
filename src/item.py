1a9c93ac80c4e6db41be459e2f1a156750e29481
Arctiqa <sinitro@yandex.ru>
2023-11-16 04:28:58 +0800

    update item.py, test_item.py

--- src/item.py
+++ src/item.py
@@ -2,8 +2,8 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate = 1.0
-    all = []
+    pay_rate = 0.85
+    all: list[str] = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,7 +13,23 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        if not isinstance(name, str):
+            raise TypeError('Неправильный тип данных для name(str)')
+        if not isinstance(price, (int, float)):
+            raise TypeError('Неправильный тип данных для unit_cost(int, float)')
+        if not isinstance(quantity, int):
+            raise TypeError('Неправильный тип данных для quantity(int)')
+        if quantity <= 0 or price <= 0:
+            raise ValueError('ValueError')
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        if name in self.all:
+            raise ValueError('Имя уже есть в списке')
+
+        self.all.append(name)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +37,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,44 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture()
+def clear_class_items():
+    Item.class_items = []
+
+
+def test_calculate_total_price(clear_class_items):
+    item1 = Item('item_name1', 30, 20)
+    item2 = Item('item_name2', 35, 2)
+    assert item1.calculate_total_price() == 600
+    assert item2.calculate_total_price() == 70
+
+
+def test_apply_discount(clear_class_items):
+    item1 = Item('item_name3', 100, 50)
+    item2 = Item('item_name4', 40, 50)
+    item1.apply_discount()
+    item2.apply_discount()
+    assert item1.price == 100 * item1.pay_rate
+    assert item2.price == 40 * item1.pay_rate
+
+
+@pytest.mark.parametrize('name, cost, quantity', [(4, 40, 40),
+                                                  ('name1', 30, 'str'),
+                                                  ('name2', '', 38)])
+def test_init_raises(name, cost, quantity):
+    with pytest.raises(TypeError):
+        Item(name, cost, quantity)
+
+
+@pytest.mark.parametrize('name, cost, quantity', [('name1', 45, -38),
+                                                  ('name2', -45, 38)])
+def test_init_value_errors(name, cost, quantity):
+    with pytest.raises(ValueError):
+        Item(name, cost, quantity)
+
+
+def test_name_already_list():
+    with pytest.raises(ValueError):
+        Item('item_name', 100, 50)
+        Item('item_name', 40, 50)
