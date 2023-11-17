079413d8e80722dcf59dd522885a3c75391aa526
Arctiqa <sinitro@yandex.ru>
2023-11-18 03:38:22 +0800

    upload homework-2

--- src/item.py
+++ src/item.py
@@ -1,40 +1,62 @@
+import csv
+import os.path
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 0.85
-    all: list[str] = []
+    all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        if not isinstance(name, str):
-            raise TypeError('Неправильный тип данных для name(str)')
-        if not isinstance(price, (int, float)):
-            raise TypeError('Неправильный тип данных для unit_cost(int, float)')
-        if not isinstance(quantity, int):
-            raise TypeError('Неправильный тип данных для quantity(int)')
-        if quantity <= 0 or price <= 0:
-            raise ValueError('ValueError')
-
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
-        if name in self.all:
-            raise ValueError('Имя уже есть в списке')
+        self.all.append(self)
 
-        Item.all.append(name)
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value[:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_file):
+        with open(os.path.join(os.path.dirname(__file__), csv_file)) as csvfile:
+            reader = list(csv.DictReader(csvfile))
+            for row in reader:
+                name, price, quantity = row.values()
+                Item(name, price, quantity)
+
+            # for row in reader:
+            #     name, price, quantity = row.values()
+            #     if name not in [i.name for i in Item.all]:
+            #         Item(name, price, quantity)
+            #         # print(Item.all)
+            #     else:
+            #         for i in Item.all:
+            #             if i.name == name:
+            #                 i.price = price
+            #                 i.quantity = quantity
+
+    @staticmethod
+    def string_to_number(value):
+        value = int(float(value))
+        return value
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
@@ -44,3 +66,4 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,44 +2,53 @@ from src.item import Item
 import pytest
 
 
-@pytest.fixture()
-def clear_class_items():
-    Item.all = []
+@pytest.fixture
+def test_object():
+    return Item('Смартфон', 50, 30)
 
 
-def test_calculate_total_price(clear_class_items):
+def test_init_(test_object):
+    assert test_object.name == 'Смартфон'
+
+    assert test_object.price == 50
+    assert test_object.quantity == 30
+    assert test_object is test_object.__class__.all[0]
+
+
+def test_calculate_total_price():
     item1 = Item('item_name1', 30, 20)
     item2 = Item('item_name2', 35, 2)
     assert item1.calculate_total_price() == 600
     assert item2.calculate_total_price() == 70
 
 
-def test_apply_discount(clear_class_items):
+def test_apply_discount():
     item1 = Item('item_name1', 100, 50)
     item2 = Item('item_name2', 40, 50)
     item1.apply_discount()
     item2.apply_discount()
     assert item1.price == 100 * item1.pay_rate
     assert item2.price == 40 * item1.pay_rate
-    Item.class_items = []
 
 
-@pytest.mark.parametrize('name, cost, quantity', [(4, 40, 40),
-                                                  ('name1', 30, 'str'),
-                                                  ('name2', '', 38)])
-def test_init_raises(name, cost, quantity):
-    with pytest.raises(TypeError):
-        Item(name, cost, quantity)
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv('items.csv')
+
+    assert Item.all[-5].name == 'Смартфон'
+    assert Item.all[-5].price == '100'
+    assert Item.all[-5].quantity == '1'
+
+    assert Item.all[-1].name == 'Клавиатура'
+    assert Item.all[-1].price == '75'
+    assert Item.all[-1].quantity == '5'
 
 
-@pytest.mark.parametrize('name, cost, quantity', [('name1', 45, -38),
-                                                  ('name2', -45, 38)])
-def test_init_value_errors(name, cost, quantity):
-    with pytest.raises(ValueError):
-        Item(name, cost, quantity)
+def test_string_to_number():
+    assert Item.string_to_number('7') == 7
+    assert Item.string_to_number('7.8') == 7
 
 
-def test_name_already_list():
-    with pytest.raises(ValueError):
-        Item('item_name', 100, 50)
-        Item('item_name', 40, 50)
+def test_name():
+    item1 = Item('Смартфон', 10500, 9)
+    item1.name = 'СуперСмартфон'
+    assert item1.name == 'СуперСмарт'
