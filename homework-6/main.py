1e0df8dd9a5c2c6b888e0ce15750d164288b3a98
Arctiqa <sinitro@yandex.ru>
2023-12-10 09:05:08 +0800

    rename

--- homework-6/main.py
+++ homework-6/main.py
@@ -2,7 +2,7 @@ from src.item import Item
 
 if __name__ == '__main__':
     # Файл broken_csv.csv отсутствует.
-    Item.instantiate_from_csv('af')
+    Item.instantiate_from_csv('not_existing_file.csv')
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле broken_csv.csv удалена последняя колонка.
